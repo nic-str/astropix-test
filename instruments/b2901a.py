0364704b5990e449fc2b0270598fbd36d77a2621
nicolas <nic.str@online.de>
2021-12-18 23:39:38 +0100

    Improved B2901A class

--- instruments/b2901a.py
+++ instruments/b2901a.py
@@ -10,6 +10,7 @@ B2901A SCPI Commands
 """
 
 class b2901a:
+
     def __init__(self, inst):
         self.inst = inst
         self.mode = "VOLT"
@@ -17,15 +18,24 @@ class b2901a:
     def setHandle(self, handle):
         self.inst = handle
 
-
     def getID(self):
-        self.inst.query("*IDN?")
+        return self.inst.query("*IDN?")
 
     def setMode(self, mode: str):
-        self.inst.write(":SOUR:FUNC:MODE "+mode)
-        self.mode = mode
+        validModes = {'VOLT', 'CURR'}
+
+        if mode in validModes:
+            self.inst.write(":SOUR:FUNC:MODE " + mode)
+            self.mode = mode
 
     def setVal(self, value: float):
+        if self.mode == "VOLT":
+            if not -210 <= value <= 210:
+                value = 0
+        elif self.mode == "CURR":
+            if not -3 <= value <= 3:
+                value = 0
+       
         self.inst.write(":SOUR:"+self.mode + " " + str(value))
 
     def setFunc(self, function: str):
@@ -33,16 +43,19 @@ class b2901a:
         validFunctions = {'VOLT', 'CURR', 'RES'}
 
         if function in validFunctions:
-            self.inst.write(":SENS:FUNC "+ function)
+            self.inst.write(":SENS:FUNC " + function)
 
     def setNPLC(self, nplc: float):
         if 0 < nplc <= 200:
-            self.inst.write(":SENS:FUNC:NPLC "+str(nplc))
+            self.inst.write(":SENS:FUNC:NPLC " + str(nplc))
 
     def setLimit(self, value: float):
-        string = ":SENS:"+"CURR"+":PROT "+str(value)
-        self.inst.write(string)
+        if self.mode == "VOLT":
+            limitmode = "CURR"
+        elif self.mode == "CURR":
+            limitmode = "VOLT"
 
+        self.inst.write(":SENS:" + limitmode + ":PROT " + str(value))
 
     def enableOut(self, enable: bool):
         if enable:
@@ -50,13 +63,12 @@ class b2901a:
         else:
             enstring = "OFF"
 
-        self.inst.write(":OUTP "+enstring)
-
+        self.inst.write(":OUTP " + enstring)
 
     def getVal(self):
 
-        return self.inst.query(":MEAS?")
+        return self.inst.query(":MEAS?").rstrip("\n")
 
     def getFunc(self):
 
-        return self.inst.query(":MEAS:FUNC?")
+        return self.inst.query(":MEAS:FUNC?").rstrip("\n")
